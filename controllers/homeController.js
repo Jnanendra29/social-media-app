15b6e7b9d927d34f6cb2e7912a3e2a55cafb2524
Jnanendra29 <jnanendrasenapati29@gmail.com>
2024-05-14 18:20:40 +0530

    added controller

---
+++ controllers/homeController.js
@@ -0,0 +1,7 @@
+const home = (req, res) => {
+  res.send('<h1>Express is up for social media app</h1>');
+};
+
+module.exports = {
+  home,
+};
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const { error } = require("console");
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
 const port = 8000;
 
-app.listen(port,() => {
-    try{
-        console.log(`server is up and running on port ${port}`)
-    }catch(error){
-        console.log("error in connection: ", error)
-    }
-})
+//use express router
+app.use("/", require("./routes"));
+
+app.listen(port, () => {
+  try {
+    console.log(`server is up and running on port ${port}`);
+  } catch (error) {
+    console.log("error in connection: ", error);
+  }
+});
